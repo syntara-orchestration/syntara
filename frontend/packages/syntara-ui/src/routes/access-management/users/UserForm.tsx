d118fbe392d109008a77fa827e5b95aca2f45260
gitdallas <5322142+gitdallas@users.noreply.github.com>
2026-09-15 17:58:22 -0500

    refactor(AAP-92059): migrate users and groups forms to SynForm

--- frontend/packages/syntara-ui/src/routes/access-management/users/UserForm.tsx
+++ frontend/packages/syntara-ui/src/routes/access-management/users/UserForm.tsx
@@ -1,20 +1,21 @@
-import { zodResolver } from '@hookform/resolvers/zod'
 import { ActionGroup, Alert, Button, Form, Stack, StackItem } from '@patternfly/react-core'
 import { RhUiAddIcon } from '@patternfly/react-icons'
 import { useNavigate } from '@tanstack/react-router'
 import type { BaseSyntheticEvent, ReactNode } from 'react'
-import type { Control } from 'react-hook-form'
-import { useForm, useWatch } from 'react-hook-form'
+import { useEffect } from 'react'
 
 import { AppRoute } from '../../../app/AppRoute'
 import { breadcrumbsCreateUser, breadcrumbsEditUser, breadcrumbsUserFormLoading } from '../../../app/breadcrumbBuilders'
 import type { AppBreadcrumbItem } from '../../../app/breadcrumbs/appBreadcrumbItem'
+import { SynForm } from '../../../components/forms/SynForm'
 import { SynPage, SynPageBody } from '../../../components/layout/SynPage'
 import { SynPageHeader } from '../../../components/layout/SynPageHeader'
 import { SynPanel } from '../../../components/layout/SynPanel'
 import { useQueryState } from '../../../components/states/useQueryState'
 import { SynPageTitle } from '../../../components/SynPageTitle'
 import { useDirtyFormGuard } from '../../../hooks/useDirtyFormGuard'
+import type { UseSynFormReturn } from '../../../hooks/useSynForm'
+import { useSynForm } from '../../../hooks/useSynForm'
 import { detachPromise } from '../../../utils/detachPromise'
 import { useDocLink } from '../../../utils/docs/useDocLink'
 import { userFormSchema, userCreateSchema, type UserFormData } from '../userFormSchema'
@@ -87,7 +88,7 @@ function userFormBreadcrumbTrail(
 }
 
 type UserFormMainPanelProps = {
-  control: Control<UserFormData>
+  form: UseSynFormReturn<UserFormData>
   isEdit: boolean
   isBuiltinUser: boolean
   isFederatedUser: boolean
@@ -98,7 +99,7 @@ type UserFormMainPanelProps = {
 }
 
 function UserFormMainPanel({
-  control,
+  form,
   isEdit,
   isBuiltinUser,
   isFederatedUser,
@@ -118,13 +119,14 @@ function UserFormMainPanel({
         {showPasswordWarning ? <PasswordWarningAlert isSelf={isSelf} /> : null}
         <StackItem>
           <Form id="user-form" onSubmit={onFormSubmit}>
-            <UserFormFields
-              control={control}
-              isEdit={isEdit}
-              isBuiltinUser={isBuiltinUser}
-              isBuiltinSelf={isBuiltinUser && isSelf}
-              isFederatedUser={isFederatedUser}
-            />
+            <SynForm form={form}>
+              <UserFormFields
+                isEdit={isEdit}
+                isBuiltinUser={isBuiltinUser}
+                isBuiltinSelf={isBuiltinUser && isSelf}
+                isFederatedUser={isFederatedUser}
+              />
+            </SynForm>
           </Form>
         </StackItem>
       </Stack>
@@ -166,17 +168,23 @@ export function UserForm({ mode }: Readonly<UserFormProps>) {
   const pageTitle = userFormPageTitle(isEdit, userQuery.data)
 
   const schema = isEdit ? userFormSchema : userCreateSchema
+  const form = useSynForm({
+    schema,
+    defaultValues: DEFAULT_VALUES,
+  })
   const {
-    control,
     handleSubmit,
     setError,
     formState: { isDirty },
     reset,
-  } = useForm<UserFormData>({
-    resolver: zodResolver(schema, undefined, { mode: 'sync' }),
-    defaultValues: formValues ?? DEFAULT_VALUES,
-    values: isEdit && formValues ? formValues : undefined,
-  })
+    watch,
+  } = form
+
+  useEffect(() => {
+    if (isEdit && formValues) {
+      reset(formValues)
+    }
+  }, [isEdit, formValues, reset])
 
   const navigateBack = () => detachPromise(navigate({ to: AppRoute.AccessManagement.Users }))
 
@@ -201,7 +209,7 @@ export function UserForm({ mode }: Readonly<UserFormProps>) {
     },
   })
 
-  const passwordValue = useWatch({ control, name: 'password' })
+  const passwordValue = watch('password')
   const showPasswordWarning = isEdit && !isFederatedUser && !!passwordValue
 
   const refetchUser = userQuery.refetch
@@ -233,7 +241,7 @@ export function UserForm({ mode }: Readonly<UserFormProps>) {
       <SynPageHeader title={pageTitle} docLink={usersDocLink} breadcrumbs={formBreadcrumbs} />
       <SynPageBody>
         <UserFormMainPanel
-          control={control}
+          form={form}
           isEdit={isEdit}
           isBuiltinUser={isBuiltinUser}
           isFederatedUser={isFederatedUser}
